d6db094650c4c64e4c21aacd1e17449700633403
kishor <kishor.shinde@enzigma.in>
2018-05-09 17:56:56 +0530

    Add Qa-Automation13

--- Specs/leadFromWeb_spec.rb
+++ Specs/leadFromWeb_spec.rb
@@ -27,8 +27,7 @@ describe "LeadGenerete" do
         @helper.addLogs('To check whether generation of lead from Website.','2016')
         @helper.addLogs('Go to Staging website and create lead')
 
-        recordJson = 
-
+        
         @driver.get "https://www-staging.wework.com/buildings/bkc--mumbai"
         @driver.find_element(:id, "tourFormContactNameField").clear
         @driver.find_element(:id, "tourFormContactNameField").send_keys @testDataJSON['CreateLeadFromWeb'][0]['Name']
@@ -52,16 +51,16 @@ describe "LeadGenerete" do
         @driver.find_element(:link, "john.sparrow [not provided]").click
         (@driver.find_element(:id, "lea2_ileinner").text).should == "john.sparrow [not provided]"
         @driver.find_element(:id, "lea11_ileinner").click
-        (@driver.find_element(:id, "lea11_ileinner").text).should == "john.sparrow123451@example.com [Gmail]"
+        (@driver.find_element(:id, "lea11_ileinner").text).should == @testDataJSON['CreateLeadFromWeb'][0]['Email'] + " [Gmail]"
         @driver.find_element(:id, "RecordType_ileinner").click
         (@driver.find_element(:id, "RecordType_ileinner").text).should == "Consumer [Change]"
         @driver.find_element(:id, "lea13_ileinner").click
         (@driver.find_element(:id, "lea13_ileinner").text).should == "Open"
-        (@driver.find_element(:id, "lea3_ileinner").text).should == "john.sparrow"
-        (@driver.find_element(:id, "lea8_ileinner").text).should == "+91-123456789"
+        (@driver.find_element(:id, "lea3_ileinner").text).should == @testDataJSON['CreateLeadFromWeb'][0]['Name']
+        (@driver.find_element(:id, "lea8_ileinner").text).should == "+91-"+ @testDataJSON['CreateLeadFromWeb'][0]['Phone']
         (@driver.find_element(:id, "lea5_ileinner").text).should == "WeWork.com"
         (@driver.find_element(:id, "00NF0000008jx4n_ileinner").text).should == "Book A Tour Availability"
-        (@driver.find_element(:id, "00N0G00000BjVWH_ileinner").text).should == "5/7/2018"
+        (@driver.find_element(:id, "00N0G00000BjVWH_ileinner").text).should == Date.today()
         (@driver.find_element(:id, "CF00NF000000DW8Sn_ileinner").text).should == "MUM-BKC"
         (@driver.find_element(:id, "00NF0000008jx61_ileinner").text).should == "MUM-BKC"
         (@driver.find_element(:id, "00N0G00000DKsrf_ileinner").text).should == "1"
@@ -70,9 +69,9 @@ describe "LeadGenerete" do
         (@driver.find_element(:link, "Vidu Mangrulkar").text).should == "Vidu Mangrulkar"
         @driver.find_element(:xpath, "//a[@id='00Q1g000002DK90_00NF000000DSUDp_link']/span").click
         @driver.find_element(:link, "john.sparrow [not provided]-Mumbai-WeWork.com").click
-        !60.times{ break if (@driver.find_element(:id, "Primary_Email__c").text == "john.sparrow123451@example.com" rescue false); sleep 1 }
-        (@driver.find_element(:id, "Primary_Email__c").text).should == "john.sparrow123451@example.com"
-        (@driver.find_element(:id, "Primary_Phone__c").text).should == "+91-123456789"
+        !60.times{ break if (@driver.find_element(:id, "Primary_Email__c").text == @testDataJSON['CreateLeadFromWeb'][0]['Email'] rescue false); sleep 1 }
+        (@driver.find_element(:id, "Primary_Email__c").text).should == @testDataJSON['CreateLeadFromWeb'][0]['Email']
+        (@driver.find_element(:id, "Primary_Phone__c").text).should == "+91-"+@testDataJSON['CreateLeadFromWeb'][0]['Phone']
         (@driver.find_element(:link, "MUM-BKC").text).should == "MUM-BKC"
         @driver.find_element(:id, "NMD_Next_Contact_Date__c").click
       @helper.postSuccessResult(2016)
